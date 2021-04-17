a61044c21db5e43967d1c76932a2805e6e4ff39a
Emre Karaman <karaman.66@hotmail.com>
2021-04-17 18:48:03 +0300

    Add project files.

---
+++ KoronaMetre/Models/KoronaBilgi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoronaMetre.Models
+{
+    public class KoronaBilgi
+    {
+        public int Id { get; set; }
+        [Required]
+        public int VakaSayisi { get; set; }
+        public int OlumSayisi { get; set; }
+        public int UlkeId { get; set; }
+        public Ulke Ulke { get; set; }
+    }
+}
---
+++ KoronaMetre/Models/Ulke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoronaMetre.Models
+{
+    public class Ulke
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Ad { get; set; }
+        public long Nufus { get; set; }
+        public KoronaBilgi KoronaBilgi { get; set; }
+
+    }
+}
